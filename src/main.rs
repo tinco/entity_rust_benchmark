46cb84204c5dca06a03b8bc69d1c7cdfe6b0bc70
Tinco Andringa <mail@tinco.nl>
2016-06-17 01:23:44 +0200

    remove big code and reduce fps size

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ extern crate sdl2;
 pub use entity_rust::{ events, tick };
 pub use std::time::{ Duration, Instant };
 pub use std::thread;
-use sdl2::rect::Rect;
+
 
 pub struct Hole {}
 
@@ -22,36 +22,6 @@ system!( tick_logger {
 		//println!("Tick!");
 	}
 });
-
-// handle the annoying Rect i32
-macro_rules! rect(
-    ($x:expr, $y:expr, $w:expr, $h:expr) => (
-        Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
-    )
-);
-
-pub fn get_centered_rect(screen_width: u32, screen_height: u32, rect_width: u32, rect_height: u32, cons_width: u32, cons_height: u32) -> Rect {
-    let wr = rect_width as f32 / cons_width as f32;
-    let hr = rect_height as f32 / cons_height as f32;
-
-    let (w, h) = if wr > 1f32 || hr > 1f32 {
-        if wr > hr {
-            //println!("Scaling down! The text will look worse!");
-            let h = (rect_height as f32 / wr) as i32;
-            (cons_width as i32, h)
-        } else {
-            //println!("Scaling down! The text will look worse!");
-            let w = (rect_width as f32 / hr) as i32;
-            (w, cons_height as i32)
-        }
-    } else {
-        (rect_width as i32, rect_height as i32)
-    };
-
-    let cx = (screen_width as i32 - w) / 2;
-    let cy = (screen_height as i32 - h) / 2;
-    rect!(cx, cy, w, h)
-}
 
 const NANOS_PER_SEC: u32 = 1_000_000_000;
 
@@ -81,6 +51,7 @@ system!( graphics {
 			use sdl2::event::{Event};
 			use sdl2::render::{Renderer, Texture, TextureQuery};
 			use std::path::Path;
+			use sdl2::rect::Rect;
 
 
 			let SCREEN_WIDTH = 800;
@@ -108,7 +79,7 @@ system!( graphics {
 			renderer.present();
 
 			let font_path = Path::new("res/SourceSansPro-Regular.ttf");
-			let mut font = ttf_context.load_font(font_path, 128).unwrap();
+			let mut font = ttf_context.load_font(font_path, 64).unwrap();
 			let text_color = Color::RGBA(255, 255, 255, 255);
 
 			let mut events = sdl_context.event_pump().unwrap();
@@ -136,20 +107,19 @@ system!( graphics {
 				let frame_duration = now - last_frame;
 				let frames_per_second = super::NANOS_PER_SEC / frame_duration.subsec_nanos();
 
-				let text = format!("Ticks: {}\nFPS: {}", ticks_per_second, frames_per_second);
+				let text = format!("Ticks: {}  FPS: {}", ticks_per_second, frames_per_second);
 
 				 // render a surface, and convert it to a texture bound to the renderer
 				let surface = font.render(&text).blended(text_color).unwrap();
 				let mut texture = renderer.create_texture_from_surface(&surface).unwrap();
 				let TextureQuery { width, height, .. } = texture.query();
 
-				let padding = 64;
-				let target = super::get_centered_rect(SCREEN_WIDTH, SCREEN_HEIGHT, width, height, SCREEN_WIDTH - padding, SCREEN_HEIGHT - padding);
+				let padding = 10;
+				let target = Rect::new(10, 10, 110, 20);
 
 				renderer.clear();
 				renderer.copy(&mut texture, None, Some(target));
 				renderer.present();
-
 			}
 
 		});
